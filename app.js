2ae22d3758da6cf8b03a30c29139111acdae405b
Vital Krasilnikau <vital.krasilnikau@gmail.com>
2017-10-01 22:03:36 +0300

    Updated to use post

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+  bodyParser = require('body-parser'),
   app = express(),
   isProd = typeof(process) !== 'undefined' && process && process.env && process.env.PORT,
   port = isProd ? process.env.PORT : 3000,
@@ -8,9 +9,16 @@ app.set('view engine', 'ejs');
 app.set('views', './www/src/views');
 
 app.use(express.static(rootFolder));
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
 
 app.get('/form', function(req, res){
   res.render('form', { query: req.query });
+});
+
+app.post('/form', function(req, res){
+  res.render('form', { query: req.body });
 });
 
 app.get('/', function(req, res){
